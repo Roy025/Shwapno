0922a61d393d8da7f23b440c09ddc58fc4673a27
Roy025 <prapti.roy025@gmail.com>
2025-04-20 02:51:30 +0600

    changed

--- backend/server.js
+++ backend/server.js
@@ -16,22 +16,6 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 
-// --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve();
-
-if (process.env.BUILD === "deployment") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
