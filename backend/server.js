ed4e2cbc3d0c8e5fbca17b188a13c5004301ae01
Roy025 <prapti.roy025@gmail.com>
2025-04-20 02:26:48 +0600

    deploy

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,22 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.BUILD === "deployment") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
